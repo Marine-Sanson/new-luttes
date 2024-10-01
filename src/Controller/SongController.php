1b4a8c41653c47b10388af260166bd7ed0aff8c5
Marine-Sanson <marine_sanson@yahoo.fr>
2024-10-01 09:39:29 +0200

    upload text file

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,21 +2,31 @@
 
 namespace App\Controller;
 
+use DateTimeZone;
 use App\Entity\Song;
+use App\Entity\Text;
 use App\Form\SongType;
+use App\Form\TextType;
+use DateTimeImmutable;
 use App\Service\SongService;
 use App\Form\SongDetailsType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 #[IsGranted('ROLE_USER')]
 #[Route('/chants')]
 class SongController extends AbstractController
 {
-    public function __construct(private readonly SongService $songService){
+    public function __construct(
+        private readonly SongService $songService,
+        private readonly SluggerInterface $slugger
+    ){
 
     }
 
@@ -43,12 +53,13 @@ class SongController extends AbstractController
     public function displaySongDetails(int $id): Response
     {
         $song = $this->songService->getSongDetails($id);
-
+        $texts = $this->songService->getSongTexts($this->songService->getSongById($id));
         // A faire *******************************************************************
         // Reste à récupérer les voix et les textes
 
         return $this->render('song/song_details.html.twig', [
             'song' => $song,
+            'texts' => $texts,
         ]);
     }
 
@@ -111,6 +122,62 @@ class SongController extends AbstractController
         return $this->render('song/update_song.html.twig', [
             'song' => $song,
             'songDetailsForm' => $songDetailsForm,
+        ]);
+    }
+
+    #[IsGranted('ROLE_CHANTS')]
+    #[Route('/addText/{id}', name: 'app_add_text_song')]
+    public function addText(int $id, Request $request, #[Autowire('%kernel.project_dir%/public/uploads/texts')] string $textDirectory): Response
+    {
+        $song = $this->songService->getSongById($id);
+        $textSongForm = $this->createForm(TextType::class);
+        $textSongForm->handleRequest($request);
+        if ($textSongForm->isSubmitted() && $textSongForm->isValid()) {
+
+            $uploadedText = $textSongForm->get(name: 'text')->getData();
+
+            if ($uploadedText) {
+
+                $originalFilename = pathinfo($uploadedText->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $this->slugger->slug($originalFilename);
+                $extension = $uploadedText->guessExtension();
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$extension;
+
+                // Move the file to the directory where brochures are stored
+                try {
+
+                    $uploadedText->move($textDirectory, $newFilename);
+                } catch (FileException $e) {
+                    echo 'Une erreur est survenue: ',  $e->getMessage(), "\n";
+                    // ... handle exception if something happens during file upload
+
+                }
+
+                $text = (new Text())
+                    ->setSong($song)
+                    ->setOriginalName($originalFilename)
+                    ->setFileName($newFilename)
+                    ->setFileType($extension)
+                    ->setCreatedAt(new DateTimeImmutable("now", new DateTimeZone("Europe/Paris")))
+                ;
+            
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $song->addText($text);
+                $this->songService->saveSongText($text);
+
+            }
+
+            // ... persist the $product variable or any other work
+
+            $this->addFlash('success', 'Nouveau chant enregistré avec succès');
+            return $this->redirectToRoute('app_manage_songs');
+        }
+
+        return $this->render('song/add_song_text.html.twig', [
+            'song' => $song,
+            'textSongForm' => $textSongForm,
         ]);
     }
 
